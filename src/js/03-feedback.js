61e1a9d82bd549782f9bf3d1cc811c387ec1577a
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-05-26 21:39:20 +0300

    DONE)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,22 +1,25 @@
 const CONTACT_FORM_LOCAL_STORAGE_KEY = 'formData';
+import throttle from 'lodash.throttle';
 const contactForm = document.querySelector('.feedback-form');
 const formData = {};
 
-const fillContactForm = form => {
-  const formDataFromLocalSorage = JSON.parse(localStorage.getItem(CONTACT_FORM_LOCAL_STORAGE_KEY));
-  const formElements = form.elements;
-  console.log(formDataFromLocalSorage);
-  console.dir(formElements);
-  for (const key in formDataFromLocalSorage) {
-    if (formDataFromLocalSorage.hasOwnProperty(key)) {
-      formElements[key].value = formDataFromLocalSorage[key];
-    }
+// const fillContactForm = form => {
+const formDataFromLocalSorage = JSON.parse(localStorage.getItem(CONTACT_FORM_LOCAL_STORAGE_KEY));
+const formElements = contactForm.elements;
+
+console.log(formDataFromLocalSorage);
+console.dir(formElements);
+
+for (const key in formDataFromLocalSorage) {
+  if (formDataFromLocalSorage.hasOwnProperty(key)) {
+    formElements[key].value = formDataFromLocalSorage[key];
   }
-};
+}
+// };
 
 // fillContactForm();
 
-const onContactFormChange = event => {
+const onContactFormChange = throttle(event => {
   const { target } = event;
 
   const contactFormValue = target.value;
@@ -24,6 +27,8 @@ const onContactFormChange = event => {
 
   formData[contactFormValueName] = contactFormValue;
   localStorage.setItem('formData', JSON.stringify(formData));
-};
+}, 500);
 
-contactForm.addEventListener('change', onContactFormChange);
+contactForm.addEventListener('input', onContactFormChange);
+
+// const formElements = form.elements;
