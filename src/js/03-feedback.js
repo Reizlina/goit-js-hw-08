0290970c142395380e0df87c202e7aec797b1ea7
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-05-27 16:07:45 +0300

    Finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,21 @@
-const CONTACT_FORM_LOCAL_STORAGE_KEY = 'formData';
 import throttle from 'lodash.throttle';
+
+const CONTACT_FORM_LOCAL_STORAGE_KEY = 'formData';
 const contactForm = document.querySelector('.feedback-form');
 const formData = {};
 
-// const fillContactForm = form => {
-const formDataFromLocalSorage = JSON.parse(localStorage.getItem(CONTACT_FORM_LOCAL_STORAGE_KEY));
-const formElements = contactForm.elements;
+const fillContactForm = form => {
+  const formDataFromLocalSorage = JSON.parse(localStorage.getItem(CONTACT_FORM_LOCAL_STORAGE_KEY));
+  const formElements = form.elements;
 
-console.log(formDataFromLocalSorage);
-console.dir(formElements);
-
-for (const key in formDataFromLocalSorage) {
-  if (formDataFromLocalSorage.hasOwnProperty(key)) {
-    formElements[key].value = formDataFromLocalSorage[key];
+  for (const key in formDataFromLocalSorage) {
+    if (formDataFromLocalSorage.hasOwnProperty(key)) {
+      formElements[key].value = formDataFromLocalSorage[key];
+    }
   }
-}
-// };
+};
 
-// fillContactForm();
+fillContactForm(contactForm);
 
 const onContactFormChange = throttle(event => {
   const { target } = event;
@@ -29,6 +27,23 @@ const onContactFormChange = throttle(event => {
   localStorage.setItem('formData', JSON.stringify(formData));
 }, 500);
 
-contactForm.addEventListener('input', onContactFormChange);
+// !_____________________
+const submitFunc = event => {
+  event.preventDefault();
+  const { email, message } = event.target;
 
-// const formElements = form.elements;
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  const obj = {
+    email: contactForm.email.value,
+    message: contactForm.message.value,
+  };
+
+  console.log(obj);
+  event.currentTarget.reset();
+  localStorage.removeItem('formData');
+};
+
+contactForm.addEventListener('submit', submitFunc);
+contactForm.addEventListener('input', onContactFormChange);
